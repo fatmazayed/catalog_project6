604f5b33b4f78e144872a6b1d4d7b8c57197b649
fatmazayed <fatmazayed@gmail.com>
2018-06-08 15:00:53 +0200

    without login engine db

--- database_setup.py
+++ database_setup.py
@@ -72,7 +72,7 @@ class CatalogItem(Base):
 
 
 def select_data():
-    sql_cmd = sqlalchemy.text(' select    cat.name||"( "||c.name||")" as name  '
+    sql_cmd = sqlalchemy.text(' select cat.name as name  '
                               'from catalog_item  cat inner join  catalog c on cat.catalog_id =c.id '
                               ' group by c.id  order by  cat.id desc,c.id   limit 10 ')
     res = engine.execute(sql_cmd).fetchall()
